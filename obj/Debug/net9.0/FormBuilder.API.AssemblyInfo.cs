024dd48cf54a24680d9482709b0d208b97bebccf
MariasolRosado <edupincay2000@gmail.com>
2026-04-23 23:43:19 -0500

    nuevo

--- obj/Debug/net9.0/FormBuilder.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/FormBuilder.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormBuilder.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3aa697cb1c06f2a497f57045416e6e1f9a2c9ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4104bb5150bc7f00e8912df88f3e5fedd11811")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormBuilder.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormBuilder.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
